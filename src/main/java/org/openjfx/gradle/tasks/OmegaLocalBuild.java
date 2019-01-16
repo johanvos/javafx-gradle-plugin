2443dbf4b8e073ca7075d2d2f48126a5245ad8e5
Johan Vos <johan.vos@gluonhq.com>
2019-01-16 15:02:28 +0100

    pass name

--- src/main/java/org/openjfx/gradle/tasks/OmegaLocalBuild.java
+++ src/main/java/org/openjfx/gradle/tasks/OmegaLocalBuild.java
@@ -4,13 +4,21 @@ import com.gluonhq.omega.Omega;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
 
+import java.util.Map;
+
 public class OmegaLocalBuild extends DefaultTask {
 
     @TaskAction
     public void action() {
-        System.err.println("OmegaLocalBuild");
+        System.err.println("OmegaLocalBuild action");
         try {
-            Omega.run();
+            final Map<String, ?> properties = getProject().getProperties();
+            String mainClassName = (String) properties.get("mainClassName");
+            String name = getProject().getName();
+            System.err.println("mcn = "+mainClassName+" and name = "+name);
+            Omega.run(mainClassName,name);
+            // System.err.println(this.getProject().getProperties());
+        //    Omega.run("com.gluonhq.svmsample", "testapp");
         } catch (Exception e) {
             e.printStackTrace();
         }
